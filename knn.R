ce269621199057ffd2760eac13309ef8e3357137
Ivelin Petrov <66024804+ivelin-petrov@users.noreply.github.com>
2023-02-18 20:42:59 +0200

    Add files via upload

---
+++ knn.R
@@ -0,0 +1,40 @@
+# setwd("E:/FMI/Курс 4, Семестър 7/Изкуствен интелект/Programs")
+
+library(class)
+
+knn_classifier = function(setK){
+  df = iris
+  new_df = df[sample(nrow(df)),]
+  
+  accuracy = 0
+  
+  for(x in 0:9){
+    training = new_df[-((15*x+1):(15*x+15)), 1:4]
+    testing = new_df[((15*x+1):(15*x+15)), 1:4]
+    
+    training_species = new_df[-((15*x+1):(15*x+15)),5]
+    testing_species = new_df[((15*x+1):(15*x+15)),5]
+    
+    classifier = knn(train = training, test = testing, cl = training_species, k = setK)
+    
+    #table(testing_species, classifier)
+    
+    m = mean(classifier == testing_species)
+    print(m)
+    
+    accuracy = accuracy + m
+  }
+  
+  print("arithmetic mean")
+  return (accuracy / 10)
+}
+
+knn_classifier(1)
+
+knn_classifier(3)
+
+knn_classifier(9)
+
+
+
+
